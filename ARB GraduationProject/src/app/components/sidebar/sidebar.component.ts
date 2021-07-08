9f75ab7b1ab4d894e149d4f81b95297b2b333230
Mohamed Omar <32581620+mo7amed3umr@users.noreply.github.com>
2021-07-08 23:55:41 +0200

    search test

--- ARB GraduationProject/src/app/components/sidebar/sidebar.component.ts
+++ ARB GraduationProject/src/app/components/sidebar/sidebar.component.ts
@@ -8,11 +8,11 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: 'table-list', title: 'Worklist',  icon:'ni-bullet-list-67 text-red', class: '' },
+    { path: 'table-list', title: 'Worklist',  icon:'ni-bullet-list-67 navy', class: '' },
     // { path: 'patient', title: 'Patient Form',  icon: 'ni-tv-2 text-primary', class: '' },
     // { path: 'preselect', title: 'Worksheet',  icon:'ni-planet text-blue', class: '' },
     // { path: 'report', title: 'Final Report',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: 'user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
+    { path: 'user-profile', title: 'User profile',  icon:'ni-single-02 navy', class: '' },
     // { path: 'viewer', title: 'Report PDF',  icon:'ni-single-02 text-yellow', class: '' }
     // { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' }
     
